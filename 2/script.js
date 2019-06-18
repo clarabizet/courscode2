8149c9523b38ab177354f382f0f5fbbb984cee88
clara <clara_bizet@yahoo.fr>
2019-06-18 15:31:57 +0200

    4eme commit js

--- 2/script.js
+++ 2/script.js
@@ -1,7 +1,14 @@
 
 $(document).ready(function(){
 	alert("Bienvenue");
-	  $( "p" ).on( "click", function() {
+
+	$("p").on( "click", function(){
+
+		if($("p").hasClass('red')){
+          $("p").removeClass('red');
+		}else{
           $("p").addClass('red');
-    });
+     	}
+
+   })
 });
