84b9bc336eac3458517058c14200845b586e1e0d
clara <clara_bizet@yahoo.fr>
2019-07-02 11:26:24 +0200

    Master Challenge

--- 2/script2.js
+++ 2/script2.js
@@ -1,7 +1,7 @@
 $(document).ready(function welcome()
 {
-	alert("Bienvenue");
-	$("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');	
+	alert("Holaaaaaaa!");
+	$("#welcome").html('<h4>Bienvenue Jeune Dauphinois, <br> es-tu prêt à relever les défis suivants pour quitter le monde étudiant ?</h4>');	
 });
 
 
